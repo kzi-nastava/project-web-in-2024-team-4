f12528169b08d5086e376538a90cc29a1f6f05cc
IsidorIvanov <isidorivanov4@hotmail.com>
2024-05-14 21:17:27 +0200

    Implementacija 1.3 funkcionalnosti

--- web-shop/src/main/java/com/webshop/controller/ProizvodController.java
+++ web-shop/src/main/java/com/webshop/controller/ProizvodController.java
@@ -1,9 +1,14 @@
 package com.webshop.controller;
 
+import com.webshop.Enumeracije.TipProdaje;
 import com.webshop.dto.ProizvodDto;
+import com.webshop.model.Kategorija;
 import com.webshop.model.Proizvod;
+import com.webshop.repository.KategorijaRepository;
 import com.webshop.repository.ProizvodRepository;
+import com.webshop.service.KategorijaService;
 import com.webshop.service.ProizvodService;
+import jakarta.annotation.Nullable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -14,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.awt.print.Pageable;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("proizvod")
@@ -22,18 +28,35 @@ public class ProizvodController {
     private ProizvodRepository proizvodRepository;
     @Autowired
     private ProizvodService proizvodService;
-
+    @Autowired
+    private KategorijaService kategorijaService;
+    @Autowired
+    private KategorijaRepository kategorijaRepository;
     @GetMapping("/lista-proizvoda")
     public ResponseEntity<List<ProizvodDto>> getAllProducts(@RequestParam(defaultValue = "0") int page,@RequestParam(defaultValue ="10")int size){
         List<ProizvodDto> proizvod=proizvodService.getAllProducts(page,size);
         return  ResponseEntity.ok(proizvod);
     }
-    @GetMapping("/proizvodi")
+    @GetMapping("/search")
     public ResponseEntity<List<ProizvodDto>> findAllByNazivContainsIgnoreCaseOrOpisContainsIgnoreCase(@RequestParam(required = false) String naziv,@RequestParam(required = false) String opis){
         List<ProizvodDto>proizvodDtos=proizvodService.findAllByNazivContainsIgnoreCaseOrOpisContainsIgnoreCase(naziv,opis);
         return  ResponseEntity.ok(proizvodDtos);
     }
-    @GetMapping("/{id}")
+
+    @GetMapping("/search/filter")
+    public ResponseEntity<List<ProizvodDto>> findProductsByFilters(
+            @RequestParam(required = false, defaultValue = "0") Double minCena,
+            @RequestParam(required = false, defaultValue = "99999999999999") Double maxCena,
+            @RequestParam(required = false) TipProdaje.tipProdaje tipProdaje,
+            @RequestParam(required = false) String nazivKategorije) {
+
+        // Pretraga proizvoda
+        List<ProizvodDto> proizvodDtos=proizvodService.findFilteredProducts(minCena, maxCena, tipProdaje, nazivKategorije);
+
+        return ResponseEntity.ok(proizvodDtos);
+    }
+
+    @GetMapping("/lista-proizvoda/{id}")
     public ResponseEntity<ProizvodDto>getOneProducts(@PathVariable long id){
         ProizvodDto proizvodDto=proizvodService.getOneProducts(id);
         return ResponseEntity.ok(proizvodDto);
